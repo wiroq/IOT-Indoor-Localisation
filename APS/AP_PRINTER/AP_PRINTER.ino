ad9cfde3661db0b6ba36c4de9895f8f0b401aec4
Halamahajne2910 <hala.mahajna@campus.technion.ac.il>
2025-05-13 17:57:07 +0300

    APS

---
+++ APS/AP_PRINTER/AP_PRINTER.ino
@@ -0,0 +1,17 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP);
+ // WiFi.softAP("ANCHOR_LOBBY", "password123");  // Set SSID and password
+  WiFi.softAP("ANCHOR_PRINTER", "password123");  // Set SSID and password
+  //WiFi.softAP("ANCHOR_HALL_ENTRANCE", "password123");  // Set SSID and password
+  //WiFi.softAP("ANCHOR_HALL_MID", "password123");  // Set SSID and password
+  //  WiFi.softAP("ANCHOR_HALL_END", "password123");  // Set SSID and password
+
+  Serial.println("Room 231 AP started");
+}
+
+void loop() {
+  // AP runs continuously
+}
