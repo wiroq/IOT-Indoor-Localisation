017a6f529ec566d756e8ef68c31d60ce305f223f
Halamahajne2910 <hala.mahajna@campus.technion.ac.il>
2025-05-13 18:14:06 +0300

    fix

--- APS/ANCHOR_SECOND_HALL_ENTRY/ANCHOR_SECOND_HALL_ENTRY.ino
+++ APS/ANCHOR_SECOND_HALL_ENTRY/ANCHOR_SECOND_HALL_ENTRY.ino
@@ -3,11 +3,12 @@
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_AP);
-  //WiFi.softAP("ANCHOR_LOBBY", "password123");  // Set SSID and password
+//  WiFi.softAP("ANCHOR_LOBBY", "password123");  // Set SSID and password
   //WiFi.softAP("ANCHOR_PRINTER", "password123");  // Set SSID and password
   //WiFi.softAP("ANCHOR_HALL_ENTRANCE", "password123");  // Set SSID and password
-  WiFi.softAP("ANCHOR_HALL_MID", "password123");  // Set SSID and password
+  //WiFi.softAP("ANCHOR_HALL_MID", "password123");  // Set SSID and password
   //  WiFi.softAP("ANCHOR_HALL_END", "password123");  // Set SSID and password
+    WiFi.softAP("ANCHOR_SECOND_HALL_ENTRY", "password123");  // Set SSID and password
 
   Serial.println("Room 231 AP started");
 }
--- APS/ANCHOR_SECOND_HALL_MID/ANCHOR_SECOND_HALL_MID.ino
+++ APS/ANCHOR_SECOND_HALL_MID/ANCHOR_SECOND_HALL_MID.ino
@@ -5,9 +5,9 @@ void setup() {
   WiFi.mode(WIFI_AP);
   //WiFi.softAP("ANCHOR_LOBBY", "password123");  // Set SSID and password
   //WiFi.softAP("ANCHOR_PRINTER", "password123");  // Set SSID and password
-//  WiFi.softAP("ANCHOR_HALL_ENTRANCE", "password123");  // Set SSID and password
-  //WiFi.softAP("ANCHOR_HALL_MID", "password123");  // Set SSID and password
-    WiFi.softAP("ANCHOR_HALL_END", "password123");  // Set SSID and password
+  //WiFi.softAP("ANCHOR_HALL_ENTRANCE", "password123");  // Set SSID and password
+  WiFi.softAP("ANCHOR_SECOND_HALL_MID", "password123");  // Set SSID and password
+  //  WiFi.softAP("ANCHOR_HALL_END", "password123");  // Set SSID and password
 
   Serial.println("Room 231 AP started");
 }
